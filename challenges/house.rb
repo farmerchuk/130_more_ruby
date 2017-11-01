9862263b5b63b4a855f0d894ba63b267fd4b8801
Jason Slusarchuk <jason.slusarchuk@gmail.com>
2017-11-01 09:56:29 -0700

    all tests pass

---
+++ challenges/house.rb
@@ -0,0 +1,145 @@
+require 'minitest/autorun'
+
+class House
+  def self.recite
+    pieces = self.new.send('pieces')
+    output = []
+
+    (pieces.size).times do |n|
+      current_pieces = pieces.slice(n..-1)
+      current_refrain = 'This is '
+
+      current_pieces.each do |el|
+        phrase1, phrase2 = el
+
+        if phrase2
+          current_refrain << phrase1 + "\n"
+          current_refrain << phrase2 + ' '
+        else
+          current_refrain << phrase1 + ".\n"
+        end
+      end
+
+      output << current_refrain
+    end
+
+    output.reverse.join("\n")
+  end
+
+  private
+
+  def pieces
+    [
+      ['the horse and the hound and the horn', 'that belonged to'],
+      ['the farmer sowing his corn', 'that kept'],
+      ['the rooster that crowed in the morn', 'that woke'],
+      ['the priest all shaven and shorn', 'that married'],
+      ['the man all tattered and torn', 'that kissed'],
+      ['the maiden all forlorn', 'that milked'],
+      ['the cow with the crumpled horn', 'that tossed'],
+      ['the dog', 'that worried'],
+      ['the cat', 'that killed'],
+      ['the rat', 'that ate'],
+      ['the malt', 'that lay in'],
+      ['the house that Jack built']
+    ]
+  end
+end
+
+# rubocop:disable Metrics/MethodLength
+class HouseTest < Minitest::Test
+  def test_rhyme
+    expected = <<-RHYME
+This is the house that Jack built.
+
+This is the malt
+that lay in the house that Jack built.
+
+This is the rat
+that ate the malt
+that lay in the house that Jack built.
+
+This is the cat
+that killed the rat
+that ate the malt
+that lay in the house that Jack built.
+
+This is the dog
+that worried the cat
+that killed the rat
+that ate the malt
+that lay in the house that Jack built.
+
+This is the cow with the crumpled horn
+that tossed the dog
+that worried the cat
+that killed the rat
+that ate the malt
+that lay in the house that Jack built.
+
+This is the maiden all forlorn
+that milked the cow with the crumpled horn
+that tossed the dog
+that worried the cat
+that killed the rat
+that ate the malt
+that lay in the house that Jack built.
+
+This is the man all tattered and torn
+that kissed the maiden all forlorn
+that milked the cow with the crumpled horn
+that tossed the dog
+that worried the cat
+that killed the rat
+that ate the malt
+that lay in the house that Jack built.
+
+This is the priest all shaven and shorn
+that married the man all tattered and torn
+that kissed the maiden all forlorn
+that milked the cow with the crumpled horn
+that tossed the dog
+that worried the cat
+that killed the rat
+that ate the malt
+that lay in the house that Jack built.
+
+This is the rooster that crowed in the morn
+that woke the priest all shaven and shorn
+that married the man all tattered and torn
+that kissed the maiden all forlorn
+that milked the cow with the crumpled horn
+that tossed the dog
+that worried the cat
+that killed the rat
+that ate the malt
+that lay in the house that Jack built.
+
+This is the farmer sowing his corn
+that kept the rooster that crowed in the morn
+that woke the priest all shaven and shorn
+that married the man all tattered and torn
+that kissed the maiden all forlorn
+that milked the cow with the crumpled horn
+that tossed the dog
+that worried the cat
+that killed the rat
+that ate the malt
+that lay in the house that Jack built.
+
+This is the horse and the hound and the horn
+that belonged to the farmer sowing his corn
+that kept the rooster that crowed in the morn
+that woke the priest all shaven and shorn
+that married the man all tattered and torn
+that kissed the maiden all forlorn
+that milked the cow with the crumpled horn
+that tossed the dog
+that worried the cat
+that killed the rat
+that ate the malt
+that lay in the house that Jack built.
+    RHYME
+    assert_equal expected, House.recite
+  end
+end
